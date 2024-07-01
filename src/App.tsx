5a0fed0be43127078adaf16692d5b23effd2ca99
rybaaa <bolshayaryba22@gmail.com>
2024-07-01 17:22:54 +0200

    tailwind added, few adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <>
-      <h1>LINGUALEARNER</h1>
+      <Homepage></Homepage>
     </>
   );
 }
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+//import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className="bg-black	h-screen	flex justify-center items-center">
+      <h1 className="text-white text-5xl font-bold">LINGUALEARNER</h1>
+    </div>
+  );
+};
+
+export default Homepage;
